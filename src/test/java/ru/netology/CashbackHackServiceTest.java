b338615fdebcbcbf1029b4c3ccc4cdfeca74d855
Nikolay <mutovin36@gmail.com>
2026-04-03 17:15:22 +0300

    Branch testng4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -2,39 +2,36 @@ package ru.netology;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-//import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
-        CashbackHackService service = new CashbackHackService();
+    CashbackHackService service = new CashbackHackService();
 
-        @Test
-        public void shouldRemain100() {
-            int amount = 900;
-            int actual = service.remain(amount);
-            int expected = 100;
+    @Test
+    public void shouldRemain100() {
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
 
-            Assert.assertEquals(actual, expected);
+        Assert.assertEquals(actual, expected);
 
-        }
+    }
 
-        @Test
-        public void shouldRemain1000() {
-            int amount = 0;
-            int actual = service.remain(amount);
-            int expected = 1000;
+    @Test
+    public void shouldRemain1000() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
 
-            Assert.assertEquals(actual, expected);
-        }
+        Assert.assertEquals(actual, expected);
+    }
 
-        @Test
-        public void shouldRemain0() {
-            int amount = 1000;
-            int actual = service.remain(amount);
-            int expected = 0;
+    @Test
+    public void shouldRemain0() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
 
-            Assert.assertEquals(actual, expected);
-        }
+        Assert.assertEquals(actual, expected);
+    }
 
 }
